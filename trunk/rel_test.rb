9a45fcd89d0acbfad6e3e44f420fa85ca640ea17
faivrem <faivrem@2c15edc5-af44-0410-ab4d-1f04e29021b0>
2008-01-27 08:20:04 +0000

    First Version

---
+++ trunk/rel_test.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+require 'treetop'
+
+class TC_MyTest < Test::Unit::TestCase
+  def setup
+    Treetop.load "rel"
+    @parser = RelParser.new
+  end
+  
+  # def teardown
+  # end
+  
+  def test_syntax
+    assert(@parser.parse('A'))
+    assert(!@parser.parse('a'))
+    assert(@parser.parse('Abc'))
+    assert(@parser.parse('A={}'))
+    assert(@parser.parse('A = { }'))
+    assert(@parser.parse('Rel={(1,2)}'))
+    assert(@parser.parse('A={(1,2),(1,2)}'))
+    assert(!@parser.parse('A={(1,2),}'))
+  end
+end
