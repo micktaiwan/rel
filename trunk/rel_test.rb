e8fe56e44dfe29180035a79cebbe5d5de38a5b5c
faivrem <faivrem@2c15edc5-af44-0410-ab4d-1f04e29021b0>
2008-01-28 00:01:20 +0000

    merged from tempo

--- trunk/rel_test.rb
+++ trunk/rel_test.rb
@@ -1,10 +1,13 @@
 require 'test/unit'
 require 'treetop'
+require 'mrel'
 
-class TC_MyTest < Test::Unit::TestCase
+class TC_REL < Test::Unit::TestCase
+  
   def setup
     Treetop.load "rel"
     @parser = RelParser.new
+    @rel = MRel.new
   end
   
   # def teardown
@@ -19,5 +22,20 @@ class TC_MyTest < Test::Unit::TestCase
     assert(@parser.parse('Rel={(1,2)}'))
     assert(@parser.parse('A={(1,2),(1,2)}'))
     assert(!@parser.parse('A={(1,2),}'))
+    assert(!@parser.parse('A={(1,)}'))
+    assert(!@parser.parse('A={(,1)}'))
+    
+    assert_equal([],@parser.parse('{}').value)
+    assert_equal([[1,2]],@parser.parse('{(1,2)}').value)
+    assert_equal([[1,2]],@parser.parse('{(1,2),(1,2)}').value)
+    assert_equal([[1,2],[2,3]],@parser.parse('{(1,2),(2,3)}').value)
   end
+  
+  def test_affect
+    assert_equal(['symbol',['A',nil]],@rel.exec('A'))
+    assert_equal([],@parser.parse('A={};A'))
+    #assert_equal([[1,2]],@parser.parse('A={(1,2)}').elements[0].value)
+    #assert_equal([[1,2],[2,3]],@parser.parse('A={(1,2),(2,3)}').elements[0].value)
+  end
+  
 end
